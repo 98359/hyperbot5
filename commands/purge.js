298363e54d58281d000d75c6140a40f6d8fc64b5
98359 <40171217+98359@users.noreply.github.com>
2018-06-20 11:02:42 +0300

    Add files via upload

---
+++ commands/purge.js
@@ -0,0 +1,10 @@
+const db = require('discord.js')
+
+exports.run =(client, message, args) => {
+
+  if(isNaN(args[0])) return message.channel.send('Please provide a valid amout to purge or delete messages!');
+  if (args[0] > 100) return message.channel.send('Supply a amout less than 100!');
+	
+  message.channel.bulkDelete(args[0])
+  message.channel.send('messages deleted succesfully :thumbsup: ')	
+};
