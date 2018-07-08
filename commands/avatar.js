7f9f8310ca70e727465468dae872511d3d8cd5ed
98359 <40171217+98359@users.noreply.github.com>
2018-07-08 12:23:56 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,13 +1,1 @@
-const Discord = require('discord.js');
 
-exports.run = (client, message, args) => {
-	let user = message.mentions.users.first() || message.author;
-		  
-		  let embed = new Discord.RichEmbed()
-		  .setAuthor(`${user.username} avatar`)
-		  .setImage(user.displayAvatarURL)
-		  .setColor('RANDOM')
-		  
-		  message.channel.send(embed)
-		 
-	   }
